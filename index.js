6d81fd8df66c4dadaa41380e5459e81da87e51d1
CHRISTOPHER WOOD <cjwood032@gmail.com>
2018-05-04 07:28:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,8 +10,17 @@ function happyHolidaysTo(name) {
 function happyHolidayTo(holiday,name) {
   return(`Happy ${holiday}, ${name}!`)
 }
+<<<<<<< HEAD
 var days=20;
 var holiday="Mother's day";
 function holidayCountdown(days,holiday) {
   return( `It's ${days} days until ${holiday}!`)
+=======
+
+function holidayCountdown(days,holiday) {
+  var days=20;
+var holiday= "Mother's day";
+  
+  return("It's ${days} days until ${holiday}!")
+>>>>>>> adbfbccb7b8a423a1337a7bce8ed88b6219e3e9d
 }
